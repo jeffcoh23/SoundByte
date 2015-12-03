4b1d2c6b2d97052020fea5cf8a8dcd39a5b5ff24
Jeff Cohen <jeffcoh23@gmail.com>
2015-12-03 13:50:45 -0500

    a working music player

--- SoundByte/TimelineViewController.swift
+++ SoundByte/TimelineViewController.swift
@@ -6,57 +6,90 @@
 //  Copyright (c) 2015 Jeff Cohen. All rights reserved.
 //
 
-import Foundation
+import AVFoundation
 import UIKit
 import Parse
+import AVKit
 
-class TimelineViewController: UIViewController{
-    var users: [PFObject] = []
+public var AudioPlayer = AVPlayer()
+public var SelectedSongNumber = Int()
+
+class TimelineViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     @IBOutlet weak var tableView: UITableView!
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    
+    var IDArray = [String]()
+    var NameArray = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
     
     
     let followingQuery = PFQuery(className: "Follow")
-    
     followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
-    //print(followingQuery.getFirstObject())
-    followingQuery.orderByAscending("username")
+        
+    let playlistFromFollowedUsers = PFQuery(className: "Playlist")
+        playlistFromFollowedUsers.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+    
+    //playlistFromFollowedUsers.includeKey("user")
+    //playlistFromFollowedUsers.orderByAscending("username")
     
     
         
-    followingQuery.findObjectsInBackgroundWithBlock {(
+    playlistFromFollowedUsers.findObjectsInBackgroundWithBlock({
         
-        result: [AnyObject]?, error: NSError?) -> Void in
+        (result: [AnyObject]?, error: NSError?) -> Void in
         
-        //print(followingQuery.getFirstObject())
-        self.users = result as? [PFObject] ?? []
-        print(self.users)
-        self.tableView.reloadData()
+        var songIDs = result as! [PFObject]
+        for i in 0...songIDs.count-1{
+            self.IDArray.append(songIDs[i].valueForKey("objectId") as! String)
+            self.NameArray.append(songIDs[i].valueForKey("songName") as! String)
+            self.tableView.reloadData()
         }
+        
+        })
+    }
+    
+    func grabSong(){
+        var SongQuery = PFQuery(className: "Playlist")
+        NSLog("\(SongQuery)")
+        SongQuery.getObjectInBackgroundWithId(IDArray[SelectedSongNumber], block: {
+            (object : PFObject?, error: NSError?) -> Void in
+            
+            //If this is a song file
+            if let AudioFileURLTemp = object?.objectForKey("songFile")?.url{
+                AudioPlayer = AVPlayer(URL: NSURL(string: AudioFileURLTemp!))
+                AudioPlayer.play()
+            }
+        })
     }
 }
 
-    extension TimelineViewController: UITableViewDataSource {
+extension TimelineViewController: UITableViewDataSource {
         
         func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-            return users.count
+            return IDArray.count
         }
+    
         func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            
-            
-            let followingQuery = PFQuery(className: "Follow")
-            
-            followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
-            //print(followingQuery.getFirstObject())
-            followingQuery.orderByAscending("username")
 
-            let cell = tableView.dequeueReusableCellWithIdentifier("PostCell")!
+            var cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! UITableViewCell
             
-            cell.textLabel!!.text = users[indexPath.row].objectId
+            cell.textLabel!.text = NameArray[indexPath.row]
             
-            return cell as! UITableViewCell
+            return cell
         }
+        
+        func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+            SelectedSongNumber = indexPath.row
+
+            grabSong()
+        }
+    
+
+    
     }
 
