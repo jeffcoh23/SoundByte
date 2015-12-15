3f803a094dd5c4c7f5523daaf2c491484b793cd6
Jeff Cohen <jeffcoh23@gmail.com>
2015-12-15 13:55:30 -0500

    code cleanup in the songSearchView controller and table

--- SoundByte/SongSearchTableViewCell.swift
+++ SoundByte/SongSearchTableViewCell.swift
@@ -11,7 +11,7 @@ import Parse
 
 protocol SongSearchTableViewCellDelegate: class {
     func cell(cell: SongSearchTableViewCell, didSelectFollowSong song: AnyObject?)
-  //  func cell(cell: SongSearchTableViewCell, didSelectUnfollowSong song: String)
+//  func cell(cell: SongSearchTableViewCell, didSelectUnfollowSong song: String)
 }
 
 class SongSearchTableViewCell: UITableViewCell {
@@ -19,40 +19,12 @@ class SongSearchTableViewCell: UITableViewCell {
     @IBOutlet weak var songSearchLabel: UILabel!
     @IBOutlet weak var addSongSearchButton: UIButton!
     weak var delegate: SongSearchTableViewCellDelegate?
-    
-    //    @IBAction func followButtonTapped(sender: AnyObject) {
-    //        if let canFollow = canFollow where canFollow == true {
-    //            delegate?.cell(self, didSelectFollowUser: user!)
-    //            self.canFollow = false
-    //        } else {
-    //            delegate?.cell(self, didSelectUnfollowUser: user!)
-    //            self.canFollow = true
-    //        }
-    //    }
-    //}
-//
+ 
+
     var songURI: AnyObject?
-//    func grabSong(){
-//        if let currentUser = PFUser.currentUser(){
-//            delegate?.cell(self,)
-//            let songQuery = PFQuery(className: "Playlist")
-//            songQuery!["spotifyTrackNumber"] =
-//        }
-//    }
-//    
+
     @IBAction func songFollowButtonTapped(sender: AnyObject) {
-        NSLog("\(songURI)")
         delegate?.cell(self, didSelectFollowSong: songURI!)
-
-
         ParseHelper.addFollowSongRelationshipToUser(songURI!, user: PFUser.currentUser()!)
-        //ParseHelper.addFollowSongRelationshipToUser(songSearchLabel.text!, user: PFUser.currentUser()!)
-//        PFUser.currentUser()?.saveInBackgroundWithBlock{
-//            succeeded, error in
-//            if succeeded{
-//                
-//            }
-//        }
-//    }
     }
 }
