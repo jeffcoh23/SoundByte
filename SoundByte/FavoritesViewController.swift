c4155c6cdf73febfb45b16d04bb711e9c21327ce
Jeff Cohen <jeffcoh23@gmail.com>
2016-02-17 00:08:47 -0500

    better working favorites section

--- SoundByte/FavoritesViewController.swift
+++ SoundByte/FavoritesViewController.swift
@@ -10,41 +10,27 @@ import UIKit
 
 class FavoritesViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
-    var songWithInfoDictionary : [String : (String, String)] = [:]{
+    var songWithInfoDictionary : [String : (String, String)] = [:]
+        {
         didSet{
-            tableView.reloadData()
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tableView.reloadData()
+            })
         }
     }
-    var songURIArray = [String](){
-        didSet{
-            tableView.reloadData()
-        }
-    }
+    var songURIArray = [String]()
+        //{
+//        didSet{
+//            tableView.reloadData()
+//        }
+//    }
     
-    func fetchLikedSongs(){
-        let pointer = PFObject(withoutDataWithClassName: "_User", objectId: PFUser.currentUser()!.objectId!)
-        var query = PFUser.query()
-        let likesQuery = PFQuery(className: "Like")
-        let finalQuery = likesQuery.whereKey("fromUser", equalTo: pointer)
-        finalQuery.findObjectsInBackgroundWithBlock ( {
-            (results: [PFObject]?, error: NSError?) -> Void in
-            if error == nil{
-                if let results = results{
-                    for result in results{
-                        self.songURIArray.append(result["likedSongURI"] as! String)
-                        self.fetchNameAndArtist(result["likedSongURI"] as! String)
-
-                    }
-                }
-            }
-            else{
-                return
-            }
-            
-            
-        })
-        
+    func fetchNewSong(notification: NSNotification){
+        let newestSong = notification.userInfo!["newLikedSong"] as! String
+        self.songURIArray.append(newestSong)
+        self.fetchNameAndArtist(newestSong)
     }
+
     
     func fetchNameAndArtist(uriTrackAsString: String!) -> [String : (String, String)]{
         let uriTrack = NSURL(string: uriTrackAsString)
@@ -59,10 +45,10 @@ class FavoritesViewController: UIViewController {
         return self.songWithInfoDictionary
     }
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
+    func getShit(){
+        self.songWithInfoDictionary.removeAll()
         let pointer = PFObject(withoutDataWithClassName: "_User", objectId: PFUser.currentUser()!.objectId!)
-        var query = PFUser.query()
+        //var query = PFUser.query()
         let likesQuery = PFQuery(className: "Like")
         let finalQuery = likesQuery.whereKey("fromUser", equalTo: pointer)
         finalQuery.findObjectsInBackgroundWithBlock({
@@ -71,17 +57,19 @@ class FavoritesViewController: UIViewController {
                 if let results = results{
                     for result in results{
                         let uriTrackAsString = result["likedSongURI"] as! String
-                        self.songURIArray.append(uriTrackAsString)
+                        
                         let uriTrack = NSURL(string: result["likedSongURI"] as! String)
                         SPTTrack.trackWithURI(uriTrack, session: nil) { (error, track) -> Void in
                             if let track = track as? SPTTrack, artist = track.artists.first as? SPTPartialArtist{
                                 self.songWithInfoDictionary.updateValue((track.name, artist.name), forKey: uriTrackAsString)
-                                
-                                
-                                
+                                self.songURIArray.append(uriTrackAsString)
+//                                dispatch_async(dispatch_get_main_queue()){
+//                                    self.tableView.reloadData()
+//                                }
                             }
                         }
                         //self.fetchNameAndArtist(result["likedSongURI"] as! String)
+                        //self.tableView.reloadData()
                         
                     }
                 }
@@ -92,9 +80,21 @@ class FavoritesViewController: UIViewController {
             
             
         })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getShit()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "fetchNewSong", name: "likeButtonClicked", object: nil)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+
         //fetchLikedSongs()
         //self.tableView.reloadData()
-        
+        //NSLog("yo")
 
         // Do any additional setup after loading the view.
     }
@@ -115,6 +115,7 @@ extension FavoritesViewController: UITableViewDataSource{
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("favoriteCell") as! FavoritesTableViewCell
+        //NSLog("\(self.songWithInfoDictionary.count)")
         var (songTitle, artistName) = self.songWithInfoDictionary[self.songURIArray[indexPath.row]]!
         cell.songName.text = songTitle
         cell.artistName.text = artistName
